f6fbf0a2f6734d394fb17a166a9f8c59230b0294
Dan Pock <self@danpock.me>
2026-04-09 10:38:06 -0400

    Add build-time version info and version sub-command

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/mallardduck/dep-fetch/internal/version"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("dep-fetch %s (commit: %s, built: %s)\n", version.Version, version.Commit, version.BuildTime)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
